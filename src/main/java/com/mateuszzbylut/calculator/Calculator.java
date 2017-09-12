8b1748d76d953dfd2e7ea9b227dc56fd835b25be
Chinet <chinetcom@gmail.com>
2017-09-12 16:56:35 +0200

    First requirement

--- src/main/java/com/mateuszzbylut/calculator/Calculator.java
+++ src/main/java/com/mateuszzbylut/calculator/Calculator.java
@@ -1,4 +1,18 @@
 package com.mateuszzbylut.calculator;
 
 public class Calculator {
+
+    private boolean status;
+
+    public Calculator() {
+        this.status = true;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public int addition(int a, int b) {
+        return a + b;
+    }
 }
--- src/test/java/com/mateuszzbylut/calculator/CalculatorTest.java
+++ src/test/java/com/mateuszzbylut/calculator/CalculatorTest.java
@@ -1,4 +1,23 @@
 package com.mateuszzbylut.calculator;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class CalculatorTest {
+
+    @Test
+    public void WhenCalculatorInitializedThenReturnTrue() {
+        Calculator calculator = new Calculator();
+
+        assertTrue(calculator.getStatus());
+    }
+
+    @Test
+    public void WhenAdditionTwoNumberThenReturnCorrectAnswer() {
+        Calculator calculator = new Calculator();
+
+        assertEquals( 5, calculator.addition(2,3));
+    }
 }
