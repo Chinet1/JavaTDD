32e3fc86030a3b4756eb5c7703e4e95d9a95cc75
Chinet <chinetcom@gmail.com>
2017-09-12 18:15:24 +0200

    Exception test

--- src/main/java/com/mateuszzbylut/calculator/Calculator.java
+++ src/main/java/com/mateuszzbylut/calculator/Calculator.java
@@ -15,4 +15,16 @@ public class Calculator {
     public int addition(int a, int b) {
         return a + b;
     }
+
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public int division(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot division by zero");
+        } else {
+            return a / b;
+        }
+    }
 }
--- src/test/java/com/mateuszzbylut/calculator/CalculatorTest.java
+++ src/test/java/com/mateuszzbylut/calculator/CalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculatorTest {
 
@@ -16,12 +17,30 @@ public class CalculatorTest {
     }
 
     @Test
-    public void WhenCalculatorInitializedThenReturnTrue() {
+    public void whenCalculatorInitializedThenReturnTrue() {
         assertTrue(calculator.getStatus());
     }
 
     @Test
-    public void WhenAdditionTwoNumberThenReturnCorrectAnswer() {
+    public void whenAdditionTwoNumberThenReturnCorrectAnswer() {
         assertEquals( 5, calculator.addition(2,3));
+    }
+
+    @Test
+    public void whenSubtractionThenReturnCorrectAnswer() {
+        assertEquals( 3, calculator.subtraction(7, 4));
+    }
+
+    @Test
+    public void whenDivisionThenReturnCorrectAnswer() {
+        assertEquals(2, calculator.division(8, 4));
+    }
+
+    @Test
+    public void whenDivisionByZeroThenThrowException() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            calculator.division(5, 0);
+        });
+        assertEquals("Cannot division by zero", exception.getMessage());
     }
 }
