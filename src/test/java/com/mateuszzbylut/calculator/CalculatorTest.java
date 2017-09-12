a8e8a008d7af0b6b26162932e96ad5dd3c1b9588
Chinet <chinetcom@gmail.com>
2017-09-12 16:57:29 +0200

    First refactoring

--- src/test/java/com/mateuszzbylut/calculator/CalculatorTest.java
+++ src/test/java/com/mateuszzbylut/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.mateuszzbylut.calculator;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -7,17 +8,20 @@ import static org.junit.Assert.assertTrue;
 
 public class CalculatorTest {
 
+    private static Calculator calculator;
+
+    @BeforeClass
+    public static void init(){
+        calculator = new Calculator();
+    }
+
     @Test
     public void WhenCalculatorInitializedThenReturnTrue() {
-        Calculator calculator = new Calculator();
-
         assertTrue(calculator.getStatus());
     }
 
     @Test
     public void WhenAdditionTwoNumberThenReturnCorrectAnswer() {
-        Calculator calculator = new Calculator();
-
         assertEquals( 5, calculator.addition(2,3));
     }
 }
