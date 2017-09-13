ccd60e726a685bfec4853f675a8fd335b35e3123
Chinet <chinetcom@gmail.com>
2017-09-13 11:49:23 +0200

    Prepering for github

--- src/test/java/com/mateuszzbylut/calculator/CalculatorTest.java
+++ src/test/java/com/mateuszzbylut/calculator/CalculatorTest.java
@@ -1,17 +1,17 @@
 package com.mateuszzbylut.calculator;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalculatorTest {
 
     private static Calculator calculator;
 
-    @BeforeClass
+    @BeforeAll
     public static void init(){
         calculator = new Calculator();
     }
